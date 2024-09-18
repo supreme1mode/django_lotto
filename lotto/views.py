6e4778191ea811b0703f3010bc63ea04f690f789
supreme1mode <supreme1mode@kyonggi.ac.kr>
2024-09-19 00:30:59 +0900

    first commit

---
+++ lotto/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import GuessNumbers
+from .forms import PostForm
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    
+
+    lottos = GuessNumbers.objects.all()
+    return render(request, 'lotto/default.html', {'lottos':lottos})
+
+
+def hello(request):
+    return HttpResponse("<h1 style='color:red;'>Hello, world!</h1>")
+
+
+def post(request):
+
+
+    if request.method == "POST":
+        print(request.POST) 
+        print(request.method) 
+      
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+	
+            lotto = form.save(commit = False) 
+            print(type(lotto))
+            print(lotto)
+            lotto.generate()
+            return redirect('index')
+    
+    else:
+        form = PostForm()
+        return render(request, "lotto/form.html", {"form": form})
+
+
+def detail(request, lottokey):
+    lotto = GuessNumbers.objects.get(pk = lottokey)
+    return render(request, "lotto/detail.html", {"lotto": lotto})
